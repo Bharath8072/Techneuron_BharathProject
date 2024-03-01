30acac6b417c92f43c37cd128a04e584e6ad7398
vasanth <vasanth@LAPTOP-2Q7CDDMQ>
2024-03-01 14:38:50 +0530

    Add project files.

---
+++ Techneuron_BharathProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Techneuron_BharathProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
